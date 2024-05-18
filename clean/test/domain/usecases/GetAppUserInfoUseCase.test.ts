612e3a6414ae7c8fc2f9d65faab9a23d73c9c5c3
ampiduxmoe <ampiduxmoe@gmail.com>
2024-05-18 23:45:23 +0500

    cover GetAppUserInfoUseCase by tests

---
+++ clean/test/domain/usecases/GetAppUserInfoUseCase.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prefer-arrow-callback */
+
+import assert = require('assert');
+import {GetAppUserInfoRequest} from '../../../src/main/domain/usecases/get_app_user_info/GetAppUserInfoRequest';
+import {GetAppUserInfoUseCase} from '../../../src/main/domain/usecases/get_app_user_info/GetAppUserInfoUseCase';
+import {VkIdConverter} from '../../../src/main/presentation/vk/VkIdConverter';
+import {OsuServer} from '../../../src/primitives/OsuServer';
+import {FakeAppUsersDao, getFakeAppUsers} from '../../mocks/AppUsersDao';
+
+describe('GetAppUserInfoUseCase', function () {
+  const osuUsers = new FakeAppUsersDao();
+  const usecase = new GetAppUserInfoUseCase(osuUsers);
+  describe('#execute()', function () {
+    it('should return AppUserInfo as undefined when user does not exist', async function () {
+      const request: GetAppUserInfoRequest = {
+        id: VkIdConverter.vkUserIdToAppUserId(-1),
+        server: OsuServer.Bancho,
+      };
+      const result = await usecase.execute(request);
+      assert.equal(result.userInfo, undefined);
+    });
+    it('should return correct AppUserInfo when user exists', async function () {
+      const existingUser = getFakeAppUsers()[0];
+      const request: GetAppUserInfoRequest = {
+        id: existingUser.id,
+        server: existingUser.server,
+      };
+      const result = await usecase.execute(request);
+      assert.equal(result.userInfo?.osuId, existingUser.osu_id);
+      assert.equal(result.userInfo?.username, existingUser.username);
+    });
+  });
+});
