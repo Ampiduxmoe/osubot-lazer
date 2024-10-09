36712182a3061811946a90fe7885c81f18b3fa3e
ampiduxmoe <ampiduxmoe@gmail.com>
2024-10-10 00:24:58 +0500

    sanitize mass ping for replies where raw user input can be used as output

--- src/main/primitives/Strings.ts
+++ src/main/primitives/Strings.ts
@@ -1,7 +1,18 @@
 export const VK_REPLY_PROCESSING = {
   mentionIdOverride: 'id1',
   sanitize: function (text: string): string {
-    const mentionRegex = /\[(?:club|id)\d+\|(.+?)\]/g;
-    return text.replace(mentionRegex, `[${this.mentionIdOverride}|$1]`);
+    const mentionRegex = /\[(?:club|id)\d+\|(.+?)\]/gi;
+    const massPingWords: string[] = [
+      ...['everyone', 'all', 'все'],
+      ...['online', 'here', 'тут', 'здесь'],
+    ];
+    const massPingPrefixes = ['@', '\\*'];
+    const massPingRegex = new RegExp(
+      `(${massPingPrefixes.join('|')})(${massPingWords.join('|')})`,
+      'gi'
+    );
+    return text
+      .replace(mentionRegex, `[${this.mentionIdOverride}|$1]`)
+      .replace(massPingRegex, `[${this.mentionIdOverride}|$1guys]`);
   },
 };
